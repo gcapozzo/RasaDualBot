2e639171bad38476ae43d32bc66cd3e133e7c5be
gianluca <gcapozzo@alumnos.exa.unicen.edu.ar>
2021-05-14 11:48:47 -0300

    added conditioned answers based on days until end of sprint

--- scrum_master/actions/actions.py
+++ scrum_master/actions/actions.py
@@ -7,21 +7,20 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_set_dias_sprint"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dias_restantes = 8.0
+
+        return [SlotSet("dias_restantes_sprint",dias_restantes)]
